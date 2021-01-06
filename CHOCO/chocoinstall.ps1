fff7ec8ec89196cbc603683bc0e0e4a3aa7a14ec
haldor65 <codyskalmowski@gmail.com>
2021-01-05 22:37:30 -0500

    added link

--- CHOCO/chocoinstall.ps1
+++ CHOCO/chocoinstall.ps1
@@ -10,7 +10,7 @@
   Last Modified     : 01/05/21
   Last Modification : added Install Command
 .Link
-  Source Location: 
+  Source Location: https://github.com/haldor65/PERSONAL/blob/main/CHOCO/chocoinstall.ps1
 #>
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol`
  -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
